fa3fd7b8a9e537821229d16a534a23b0f6a6d84f
kazuikios <slash0juice@gmail.com>
2022-04-14 11:56:50 +0900

    death heartbeat

--- concurrency/p164_heartbeat.go
+++ concurrency/p164_heartbeat.go
@@ -50,25 +50,73 @@ func doWork(done <-chan interface{}, pulseInterval time.Duration) (<-chan interf
 	return heartbeat, results
 }
 
+func doWorkBad(done <-chan interface{}, pulseInterval time.Duration) (<-chan interface{}, <-chan time.Time) {
+	heartbeat := make(chan interface{}) // 1
+	results := make(chan time.Time)
+
+	go func() {
+		defer close(heartbeat)
+		defer close(results)
+
+		pulse := time.Tick(pulseInterval)       // 2
+		workGen := time.Tick(2 * pulseInterval) // 3
+
+		sendPulse := func() {
+			select {
+			case heartbeat <- struct{}{}:
+			default: // 4
+			}
+		}
+		sendResult := func(r time.Time) {
+			for {
+				select {
+				case <-done:
+					return
+				case <-pulse: // 5
+					sendPulse()
+				case results <- r:
+					return
+				}
+			}
+		}
+
+		for i := 0; i < 2; i++ {
+			select {
+			case <-done:
+				return
+			case <-pulse: // 5
+				sendPulse()
+			case r := <-workGen:
+				sendResult(r)
+			}
+		}
+	}()
+
+	return heartbeat, results
+}
+
 func P164HeartBeat() {
 	done := make(chan interface{})
 	time.AfterFunc(10*time.Second, func() { close(done) })
 
 	const timeout = 2 * time.Second
-	heartbeat, results := doWork(done, timeout/2)
+	heartbeat, results := doWorkBad(done, timeout/2)
 	for {
 		select {
 		case _, ok := <-heartbeat:
 			if !ok {
+				fmt.Println("heartbeat die")
 				return
 			}
 			fmt.Println("pulse")
 		case r, ok := <-results:
-			if ok == false {
+			if !ok {
+				fmt.Println("results die")
 				return
 			}
 			fmt.Printf("results %v\n", r.Second())
 		case <-time.After(timeout):
+			fmt.Println("worker goroutine is not healthy!")
 			return
 		}
 	}
